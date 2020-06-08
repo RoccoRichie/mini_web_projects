e131377c749480c1804f17e8abcbeb9588dd3df8
RoccoRichie <richieloughran@gmail.com>
2020-06-08 19:46:43 +0100

    Basic form validation which needs to be refactored

---
+++ form-validator/script.js
@@ -0,0 +1,67 @@
+// Basic Validation
+// TODO: Refactor into functions
+
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Show input error message
+function showError(input, message) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerText = message;
+}
+
+// Show Success outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
+}
+
+// Check email is valid
+function isValidEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+// Add an Event listener
+form.addEventListener('submit', function (e) {
+  // Prevent it from submitting
+  e.preventDefault();
+
+  console.log('Submit clicked');
+  console.log(username.value);
+
+  // username validation
+  if (username.value === '') {
+    showError(username, 'Username is required');
+  } else {
+    // Normal we would just submit the form and have backend handle it
+    showSuccess(username);
+  }
+  // email validation
+  if (email.value === '') {
+    showError(email, 'Email is required');
+  } else if (!isValidEmail(email.value)) {
+    showError(email, 'Email is not valid');
+  } else {
+    showSuccess(email);
+  }
+
+  // password validation
+  if (password.value === '') {
+    showError(password, 'Password is required');
+  } else {
+    showSuccess(password);
+  }
+
+  // password2 validation
+  if (password2.value === '') {
+    showError(password2, 'Password2 is required');
+  } else {
+    showSuccess(password2);
+  }
+});

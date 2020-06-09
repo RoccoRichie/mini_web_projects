4a91aa5827618267befde4ac8b11bf73b83c484a
RoccoRichie <richieloughran@gmail.com>
2020-06-09 11:45:08 +0100

    Movie Seat Booker - JS WIP - Seats Selected, count & total

--- movie_seat_booker/script.js
+++ movie_seat_booker/script.js
@@ -1,26 +1,34 @@
-// querySelector - Selects one (1st found)
-// querySelectorAll - Selects all and stores in an array (node list)
 const container = document.querySelector('.container');
 // Select all seats from a row not occupied
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
-
-// Get the ticket price from the selected movie
+// + beside it converts it to a number
 const ticketPrice = +movieSelect.value;
 
-console.log(container);
-console.log(seats);
-console.log(count);
-console.log(total);
-console.log(movieSelect);
-console.log(ticketPrice);
-console.log(typeof ticketPrice);
+// Update count and total
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  const selectedSeatsCount = selectedSeats.length;
+  // console.log(selectedSeatsCount);
+  // Update the count text
+  count.innerText = selectedSeatsCount;
+  // update the total: selectedSeatsCount * ticketPrice
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
 
-// Add an Event listener - Clicking on a seat - change class
-//  Add event listener on the container and check if the click is on a seat => Use arrow function
 container.addEventListener('click', (e) => {
   // This will show what is element is clicked on within the container
-  console.log(e.target);
+  // console.log(e.target);
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    // console.log('Available seat selected');
+    // Add 'selected' class(toggle -on/off)
+    e.target.classList.toggle('selected');
+    // Update the count in the span text(dom => count)
+    updateSelectedCount();
+  }
 });

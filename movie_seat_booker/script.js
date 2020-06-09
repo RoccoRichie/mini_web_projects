30527bd1be1c2ed82f864a18d0557bc9a75727f9
RoccoRichie <richieloughran@gmail.com>
2020-06-09 15:38:17 +0100

    Movie Seat Booker - JS WIP - Local Storeage retrival of seats

--- movie_seat_booker/script.js
+++ movie_seat_booker/script.js
@@ -1,10 +1,11 @@
 const container = document.querySelector('.container');
-// Select all seats from a row not occupied
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
-// + beside it converts it to a number
+
+populateUI();
+
 let ticketPrice = +movieSelect.value;
 
 // Save Selected Movie index and price
@@ -16,50 +17,43 @@ function setMovieData(movieIndex, moviePrice) {
 // Update count and total
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
-
-  // Copy selected seats to an array
-  // Map through the array (high order array map => similar to for each but returns an array), return the index of dom element seats (as above)
-  // return a new array of indexes => spread operator [...]
-
-  // const seatsIndex = [...selectedSeats].map((seat) => {
-  //   return [...seats].indexOf(seat);
-  // });
-  // Because there is only one expression we can remove the return keyword and curleys
   const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
-  // console.log(seatsIndex);
 
   // Save to local storeage - String - Covert array to JSON.stringify
   localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
   const selectedSeatsCount = selectedSeats.length;
-  // console.log(selectedSeatsCount);
-  // Update the count text
   count.innerText = selectedSeatsCount;
-  // update the total: selectedSeatsCount * ticketPrice
   total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Get data from localStorage and populate UI
+function populateUI() {
+  // Parse it back into an Array
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
 }
 
 // Movie select event
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
-  // console.log(ticketPrice);
-  // console.log(e.target.selectedIndex, e.target.value);
   setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
 // Seat click Event
 container.addEventListener('click', (e) => {
-  // This will show what is element is clicked on within the container
-  // console.log(e.target);
   if (
     e.target.classList.contains('seat') &&
     !e.target.classList.contains('occupied')
   ) {
-    // console.log('Available seat selected');
-    // Add 'selected' class(toggle -on/off)
     e.target.classList.toggle('selected');
-    // Update the count in the span text(dom => count)
     updateSelectedCount();
   }
 });

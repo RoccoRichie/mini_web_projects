0f028d6d5a854ede87472ab90c44d6ba7ac86d3a
RoccoRichie <richieloughran@gmail.com>
2020-06-09 14:13:32 +0100

    Movie Seat Booker - JS WIP - Local Storeage for seat and movie index and movie price

--- movie_seat_booker/script.js
+++ movie_seat_booker/script.js
@@ -7,9 +7,30 @@ const movieSelect = document.getElementById('movie');
 // + beside it converts it to a number
 let ticketPrice = +movieSelect.value;
 
+// Save Selected Movie index and price
+function setMovieData(movieIndex, moviePrice) {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMovieProce', moviePrice);
+}
+
 // Update count and total
 function updateSelectedCount() {
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  // Copy selected seats to an array
+  // Map through the array (high order array map => similar to for each but returns an array), return the index of dom element seats (as above)
+  // return a new array of indexes => spread operator [...]
+
+  // const seatsIndex = [...selectedSeats].map((seat) => {
+  //   return [...seats].indexOf(seat);
+  // });
+  // Because there is only one expression we can remove the return keyword and curleys
+  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+  // console.log(seatsIndex);
+
+  // Save to local storeage - String - Covert array to JSON.stringify
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
   const selectedSeatsCount = selectedSeats.length;
   // console.log(selectedSeatsCount);
   // Update the count text
@@ -22,6 +43,8 @@ function updateSelectedCount() {
 movieSelect.addEventListener('change', (e) => {
   ticketPrice = +e.target.value;
   // console.log(ticketPrice);
+  // console.log(e.target.selectedIndex, e.target.value);
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 });
 
